e24a1852daa4a35fa94c5ad0a5ef850871db5c71
Geyer, Lara <lara.geyer@spiegel.de>
2025-05-10 17:29:17 +0200

    adjusted database configuration

--- app/__init__.py
+++ app/__init__.py
@@ -9,15 +9,18 @@ login_manager = LoginManager()
 mail = Mail()
 
 def create_app():
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    db_path = os.environ.get("DATABASE_URL") or os.path.join("/tmp", "db.db")
-
     app = Flask(__name__, template_folder="templates")
     app.secret_key = 'abcd'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+
+    database_url = os.environ.get("DATABASE_URL")
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    elif not database_url:
+        database_url = f"sqlite:///{os.path.join('/tmp', 'db.db')}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+    app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
     app.config['MAIL_PORT'] = 2525
     app.config['MAIL_DEFAULT_SENDER'] = 'laramariegeyer@gmail.com'
     app.config['MAIL_USERNAME'] = '8afe4e49962d19'
@@ -25,13 +28,13 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
 
-    """app.config['MAIL_SERVER'] = 'live.smtp.mailtrap.io'
-    app.config['MAIL_DEFAULT_SENDER'] = 'hello@lara-geyer.de'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USERNAME'] = 'api'
-    app.config['MAIL_PASSWORD'] = '87e7b0cdbed96809f87c6bbf36582b3b'
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False"""
+    # app.config['MAIL_SERVER'] = 'live.smtp.mailtrap.io'
+    # app.config['MAIL_DEFAULT_SENDER'] = 'hello@lara-geyer.de'
+    # app.config['MAIL_PORT'] = 587
+    # app.config['MAIL_USERNAME'] = 'api'
+    # app.config['MAIL_PASSWORD'] = '...'
+    # app.config['MAIL_USE_TLS'] = True
+    # app.config['MAIL_USE_SSL'] = False
 
     db.init_app(app)
     login_manager.init_app(app)
@@ -48,8 +51,8 @@ def create_app():
     from .routes import bp as main_bp
     app.register_blueprint(main_bp)
 
-    from .models import User, Message
     with app.app_context():
+        from .models import User, Message
         db.create_all()
 
     return app
