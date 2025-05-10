3eb571d68844136882d0be84d587073a6af0af9d
Geyer, Lara <lara.geyer@spiegel.de>
2025-05-10 16:34:28 +0200

    adjusted model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,15 @@
 from . import db
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+
     username = db.Column(db.String(200), unique=True, nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    is_verified = db.Column(db.Boolean, default=False)
 
     sent_messages = db.relationship(
         "Message",
@@ -16,6 +22,13 @@ class User(db.Model):
         foreign_keys='Message.recipient_id',
         lazy=True
     )
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
